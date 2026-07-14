45fe58e4322625f584a6f5a854a4c2f8c651cb78
tqbinh1268 <tqbinh@gmail.com>
2026-07-14 13:32:25 +0700

    Hoan thanh Lab2 Bai 1

---
+++ Lab2Ca3/Bai1_HinhHoc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define PI 3.14159
+const float PII = 3.14159;
+
+int main(){
+    //Nhap dai, rong tinh chi vi, dien tich hcn
+    float dai, rong, chuViHCN, dienTichHCN,r,chuViHT, dienTichHT;
+    printf("Nhap chieu dai, chieu rong: ");
+    scanf("%f%f",&dai,&rong);
+    chuViHCN = (dai + rong)*2;
+    dienTichHCN = dai*rong;
+    printf("Hinh chu nhat\nDai: %.2f\nRong: %.2f\nChu vi: %.2f\nDien tich: %.2f\n",dai,rong,chuViHCN,dienTichHCN);
+    
+    printf("Nhap ban kinh: ");
+    scanf("%f",&r);
+    chuViHT = r*2*PII;  //duong kinh * PI
+    dienTichHT = r*r*PI;
+    printf("Hinh chu nhat\nBan kinh: %.2f\nChu vi: %.2f\nDien tich: %.2f\n",r,chuViHT,dienTichHT);
+    return 0;
+}
